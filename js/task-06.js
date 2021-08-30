884f378d62c8e19c3c6ff6de9f291af43dcfe435
Dima Sotskiy <sotskiydr@gmail.com>
2021-08-31 00:29:39 +0300

    nicho strashnogo

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const dataLength = input.getAttribute('data-length');
 input.addEventListener('blur', onBlurInputArea);
 
 function onBlurInputArea (){
-    if(dataLength < input.value.length){
+    if(dataLength <= input.value.length){
         input.classList.remove('invalid');
         input.classList.add('valid');
     }else{
