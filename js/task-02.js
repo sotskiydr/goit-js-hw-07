44c44cc6d954cc90120bc9ad662f474777060702
Dima Sotskiy <sotskiydr@gmail.com>
2021-08-31 12:55:22 +0300

    Home-Work

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const ul = document.querySelector('#ingredients');
+const currentList = document.querySelector('#ingredients');
+const newList = document.createElement('ul');
+
 ingredients.forEach(elem => {
   const li = document.createElement('li');
   li.textContent = elem;
-  ul.appendChild(li)
+  newList.appendChild(li)
 });
-
-
+currentList.replaceWith(newList);
