7471216766a1940993eacc3b52554a3e3f421534
Dima Sotskiy <sotskiydr@gmail.com>
2021-08-31 16:57:14 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,11 @@ const dataLength = input.getAttribute('data-length');
 input.addEventListener('blur', onBlurInputArea);
 
 function onBlurInputArea (){
-    if(dataLength <= input.value.length){
-        input.classList.remove('invalid');
-        input.classList.add('valid');
-    }else{
+    if(dataLength != input.value.length){
         input.classList.remove('valid');
         input.classList.add('invalid');
+    }else{
+        input.classList.remove('invalid');
+        input.classList.add('valid');
     }
 }
